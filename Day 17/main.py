8eb873dec525d2c4f73d25d7b98fcbe08c539c71
Aaditya Kharkia <aadityakharkia10@gmail.com>
2022-12-28 19:22:58 +0530

    Updated

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,4 +1,9 @@
-class Question:
-    def __init__(self,q_text,q_answer):
-        self.text = q_text
-        self.answer = q_answer
+from question_model import Question
+from data import question_data
+
+question_bank = []
+for i in question_data:
+    question_text = i["text"]
+    question_answer = i["answer"]
+    new_question = Question(question_text,question_answer)
+    question_bank.append(new_question)
