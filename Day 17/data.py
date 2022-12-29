1dd2f0c5ace185952b948430976db2b4fcf34aec
Aaditya Kharkia <aadityakharkia10@gmail.com>
2022-12-29 21:33:42 +0530

    Updated

--- Day 17/data.py
+++ Day 17/data.py
@@ -1,7 +1,7 @@
 question_data = [
     {"text": "A slug's blood is green.", "answer": "True"},
     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+    {"text": "Appfroximately one quarter of human bones are in the feet.", "answer": "True"},
     {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
     {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
     {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
