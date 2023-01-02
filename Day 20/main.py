88b0555e58d7f8def0a460709824d2ebe9431698
Aaditya Kharkia <aadityakharkia10@gmail.com>
2023-01-02 20:45:17 +0530

    Updated

--- Day 20/main.py
+++ Day 20/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
-import Snake
+
 
 screen = Screen()
 screen.setup(height=600,width=600)
@@ -10,16 +11,11 @@ screen.tracer(0)
 
 snake = Snake()
 
-
 is_on = True
 while is_on == True:
     screen.update()
     time.sleep(0.3)
-    for i in range(len(segment)-1,0,-1):
-        x= segment[i - 1].xcor()
-        y=segment[i - 1].ycor()
-        segment[i].goto(x,y)
-    segment[0].forward(10)
-    segment[0].left(90)
+
+    snake.move()
 
 screen.exitonclick()
