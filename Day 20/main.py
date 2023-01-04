dfd793a76c05523783240b6aa79743439636c9e7
Aaditya Kharkia <aadityakharkia10@gmail.com>
2023-01-04 19:31:50 +0530

    Updated

--- Day 20/main.py
+++ Day 20/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 import time
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 
 screen.listen()
@@ -21,8 +23,10 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    if snake.head.distance(food)< 15:
+        food.refresh()
 
 
 screen.exitonclick()
