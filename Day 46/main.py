296a47a9d4e5d541a75411ea2d46c6a8e6cbc4f9
Aaditya Kharkia <aadityakharkia10@gmail.com>
2023-01-30 22:20:17 +0530

    Updated

---
+++ Day 46/main.py
@@ -0,0 +1,51 @@
+# One of the Hardest Project
+
+# Using spotipy API to access Spotify
+
+import requests
+from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+URL = "https://www.billboard.com/charts/hot-100/"
+date = input(f"Please Enter the Date in the formate of YYYY-MM-DD\n")
+
+Spotify_Clint_id = "f34c2de437994f4095b0665fef6e3995"
+Spotify_Clint_Secret = "672bc2fd45884de39ff11bbfc649a5ff"
+Spotify_API_URL = "https://api.spotify.com/v1"
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=Spotify_Clint_id,
+        client_secret=Spotify_Clint_Secret,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+
+
+response = requests.get(URL + date)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_spans = soup.find_all("span", class_="chart-element__information__song")
+song_names = [song.getText() for song in song_names_spans]
+
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+print(playlist)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
